645587e2f00b2ff2b74672f6074ec25c64586994
Jerome <ducampdorgasjerome@gmail.com>
2021-10-19 14:23:38 +0200

    header component + sass

---
+++ admin/src/components/header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <div id="banniere">
+      <img src="../assets/logo.png" alt="logo orinoco" id="logo" />
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="scss">
+header {
+  margin: auto;
+  max-width: 100%;
+  height: 400px;
+  & img {
+    width: 100%;
+    height: 400px;
+  }
+}
+</style>
